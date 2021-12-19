7be81d613d91c689e307f7501fb76b24a19b8646
RomanDiachuk <rwdiachuk@gmail.com>
2021-12-19 18:51:33 +0200

    one more refactor

--- src/main/java/com/example/binanceparser/algorithm/TestSpotBalanceCalcAlgorithm.java
+++ src/main/java/com/example/binanceparser/algorithm/TestSpotBalanceCalcAlgorithm.java
@@ -1,117 +1,116 @@
 package com.example.binanceparser.algorithm;
 
-import static com.example.binanceparser.Constants.USDT;
-import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
+import com.example.binanceparser.Constants;
+import com.example.binanceparser.config.BalanceVisualizerConfig;
+import com.example.binanceparser.domain.Asset;
+import com.example.binanceparser.domain.SpotIncomeState;
+import com.example.binanceparser.domain.events.*;
+
 import java.math.BigDecimal;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import com.example.binanceparser.Constants;
-import com.example.binanceparser.config.BalanceVisualizerConfig;
-import com.example.binanceparser.domain.Asset;
-import com.example.binanceparser.domain.SpotIncomeState;
-import com.example.binanceparser.domain.events.AbstractEvent;
-import com.example.binanceparser.domain.events.AccountPositionUpdateEvent;
-import com.example.binanceparser.domain.events.BalanceUpdateEvent;
-import com.example.binanceparser.domain.events.EventType;
-import com.example.binanceparser.domain.events.OrderTradeUpdateEvent;
+
+import static com.example.binanceparser.Constants.USDT;
+import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
 
 public class TestSpotBalanceCalcAlgorithm implements CalculationAlgorithm<SpotIncomeState> {
-	private final BalanceVisualizerConfig config;
-	private final int MAX_SECONDS_DELAY_FOR_VALID_EVENTS = 1;
-	
-	public TestSpotBalanceCalcAlgorithm(BalanceVisualizerConfig config) {
-		this.config = config;
-	}
+    private final BalanceVisualizerConfig config;
+    private final int MAX_SECONDS_DELAY_FOR_VALID_EVENTS = 1;
 
-	@Override
-	public List<SpotIncomeState> processEvents(List<AbstractEvent> events) {
-		return processEvents(events, config.getAssetsToTrack());
-	}
+    public TestSpotBalanceCalcAlgorithm(BalanceVisualizerConfig config) {
+        this.config = config;
+    }
 
-	@Override
-	public List<SpotIncomeState> processEvents(List<AbstractEvent> events, List<String> assetsToTrack) {
-		final List<SpotIncomeState> spotIncomeStates = new ArrayList<>();
-		for (int i = 0; i < events.size() - 1; i++) {
-			final AbstractEvent currentEvent = events.get(i);
-			final AbstractEvent nextEvent = events.get(i + 1);
+    @Override
+    public List<SpotIncomeState> processEvents(List<AbstractEvent> events) {
+        return processEvents(events, config.getAssetsToTrack());
+    }
 
-			if ((currentEvent.getEventType() != EventType.ORDER_TRADE_UPDATE
-					&& currentEvent.getEventType() != EventType.BALANCE_UPDATE)
-					|| nextEvent.getEventType() != EventType.ACCOUNT_POSITION_UPDATE) {
-				continue;
-			}
+    @Override
+    public List<SpotIncomeState> processEvents(List<AbstractEvent> events, List<String> assetsToTrack) {
+        final List<SpotIncomeState> spotIncomeStates = new ArrayList<>();
+        for (int i = 0; i < events.size() - 1; i++) {
+            final AbstractEvent currentEvent = events.get(i);
+            final AbstractEvent nextEvent = events.get(i + 1);
 
-			if (ChronoUnit.SECONDS.between(currentEvent.getDateTime(),
-					nextEvent.getDateTime()) > MAX_SECONDS_DELAY_FOR_VALID_EVENTS) {
-				continue;
-			}
+            if ((currentEvent.getEventType() != EventType.ORDER_TRADE_UPDATE
+                    && currentEvent.getEventType() != EventType.BALANCE_UPDATE)
+                    || nextEvent.getEventType() != EventType.ACCOUNT_POSITION_UPDATE) {
+                continue;
+            }
 
-			//TODO FOR NOW VERY QUESTIONABLE HOW TO HANDLE
-			if (currentEvent.getEventType() == EventType.BALANCE_UPDATE) {
-				final BalanceUpdateEvent balanceEvent = (BalanceUpdateEvent) currentEvent;
-				SpotIncomeState incomeState = new SpotIncomeState(balanceEvent.getDateTime());
-				incomeState.updateAssetState(balanceEvent.getBalances(), balanceEvent.getBalanceDelta(), null);
-				//spotIncomeStates.add(incomeState); TODO
-				continue;
-			}
+            if (ChronoUnit.SECONDS.between(currentEvent.getDateTime(),
+                    nextEvent.getDateTime()) > MAX_SECONDS_DELAY_FOR_VALID_EVENTS) {
+                continue;
+            }
 
-			final OrderTradeUpdateEvent orderEvent = (OrderTradeUpdateEvent) currentEvent;
-			final AccountPositionUpdateEvent accEvent = (AccountPositionUpdateEvent) nextEvent;
-			final String orderSymbol = orderEvent.getSymbol().replace(USDT, "");
+            //TODO FOR NOW VERY QUESTIONABLE HOW TO HANDLE
+            if (currentEvent.getEventType() == EventType.BALANCE_UPDATE) {
+                final BalanceUpdateEvent balanceEvent = (BalanceUpdateEvent) currentEvent;
+                SpotIncomeState incomeState = new SpotIncomeState(balanceEvent.getDateTime());
+                incomeState.processOrderDetails(balanceEvent.getBalances(), balanceEvent.getBalanceDelta(), null);
+                //spotIncomeStates.add(incomeState); TODO
+                continue;
+            }
 
-			if (!orderEvent.getOrderStatus().equals("FILLED")
-					|| (!assetsToTrack.contains(orderSymbol) && assetsToTrack.size() != 0)) {
-				continue;
-			}
-			
-			logTrade(orderEvent);
-			
-			SpotIncomeState incomeState = spotIncomeStates.size() == 0 ? 
-					new SpotIncomeState(currentEvent.getDateTime())
-					:
-					new SpotIncomeState(currentEvent.getDateTime(), spotIncomeStates.get(spotIncomeStates.size() - 1));
-			
-			incomeState.updateCurrentAssets(accEvent.getBalances().stream()
+            final OrderTradeUpdateEvent orderEvent = (OrderTradeUpdateEvent) currentEvent;
+            final AccountPositionUpdateEvent accEvent = (AccountPositionUpdateEvent) nextEvent;
+            final String orderSymbol = orderEvent.getSymbol().replace(USDT, "");
+
+            if (!orderEvent.getOrderStatus().equals("FILLED")
+                    || (!assetsToTrack.contains(orderSymbol) && assetsToTrack.size() != 0)) {
+                continue;
+            }
+
+            logTrade(orderEvent);
+
+            final SpotIncomeState incomeState = spotIncomeStates.size() == 0 ?
+                    new SpotIncomeState(currentEvent.getDateTime())
+                    : new SpotIncomeState(currentEvent.getDateTime(), spotIncomeStates.get(spotIncomeStates.size() - 1));
+
+            //update asset balances
+			final List<Asset> updatedAssets = accEvent.getBalances().stream()
 					.filter(asset -> assetsToTrack.contains(asset.getAsset()) || assetsToTrack.size() == 0)
 					.map(asset -> new Asset(asset.getAsset(), asset.getFree().add(asset.getLocked())))
-					.collect(Collectors.toList()));
+					.collect(Collectors.toList());
+			incomeState.updateAssetBalance(updatedAssets);
+			//track user's absolute trade performance
+            if (orderEvent.getSide().equals("BUY")) {
+                incomeState.processOrderDetails(orderSymbol, orderEvent.getOriginalQuantity(), includeCommissionToBuyPrice(orderEvent));
+            } else if (orderEvent.getSide().equals("SELL")) {
+                incomeState.processOrderDetails(orderSymbol, orderEvent.getOriginalQuantity().negate(), includeCommissionToSellPrice(orderEvent));
+            } else {
+                throw new IllegalArgumentException("Unrecognized order.Side");
+            }
 
-			if(orderEvent.getSide().equals("BUY")) {
-				incomeState.updateAssetState(orderSymbol, orderEvent.getOriginalQuantity(), includeCommissionToBuyPrice(orderEvent));
-			} else if(orderEvent.getSide().equals("SELL")) {
-				incomeState.updateAssetState(orderSymbol, orderEvent.getOriginalQuantity().negate(), includeCommissionToSellPrice(orderEvent));
-			} else {
-				throw new IllegalArgumentException("Unrecognized order.Side");
-			}
-			
-			spotIncomeStates.add(incomeState);	
-		}
-		return spotIncomeStates;
-	}
+            spotIncomeStates.add(incomeState);
+        }
+        return spotIncomeStates;
+    }
 
 
-	//TODO чого в одному випадку ми віднімаємо комісію а в іншому додаємо?
-	// ми ж  в обох випадках ПЛАТИМО комісію біржі. Думаю потрібно завжди додавати (?)
-	private BigDecimal includeCommissionToBuyPrice(OrderTradeUpdateEvent orderEvent) {
-		return orderEvent.getPrice()//COMISSION WORKS ONLY FOR USDT NOW
-				.add(orderEvent.getCommission().divide(orderEvent.getOriginalQuantity(), Constants.MATH_CONTEXT));
-	}
-	
-	private BigDecimal includeCommissionToSellPrice(OrderTradeUpdateEvent orderEvent) {
-		return orderEvent.getPrice()//COMISSION WORKS ONLY FOR USDT NOW
-				.subtract(orderEvent.getCommission().divide(orderEvent.getOriginalQuantity(), Constants.MATH_CONTEXT));
-	}
-	
-	private void logTrade(OrderTradeUpdateEvent orderEvent) {
-		final BigDecimal quoteAssetQty = orderEvent.getOriginalQuantity()
-				.multiply(orderEvent.getPriceOfLastFilledTrade());
-		final String str = String.format("%s %s %s %s for total of %s quoteAsset",
-				orderEvent.getDateTime().format(ISO_DATE_TIME), orderEvent.getSide(),
-				orderEvent.getOriginalQuantity().toPlainString(), orderEvent.getSymbol(),
-				quoteAssetQty.toPlainString());
-		System.out.println(str);
-	}
+    //TODO чого в одному випадку ми віднімаємо комісію а в іншому додаємо?
+    // ми ж  в обох випадках ПЛАТИМО комісію біржі. Думаю потрібно завжди додавати (?)
+    private BigDecimal includeCommissionToBuyPrice(OrderTradeUpdateEvent orderEvent) {
+        return orderEvent.getPrice()//COMISSION WORKS ONLY FOR USDT NOW
+                .add(orderEvent.getCommission().divide(orderEvent.getOriginalQuantity(), Constants.MATH_CONTEXT));
+    }
+
+    private BigDecimal includeCommissionToSellPrice(OrderTradeUpdateEvent orderEvent) {
+        return orderEvent.getPrice()//COMISSION WORKS ONLY FOR USDT NOW
+                .subtract(orderEvent.getCommission().divide(orderEvent.getOriginalQuantity(), Constants.MATH_CONTEXT));
+    }
+
+    private void logTrade(OrderTradeUpdateEvent orderEvent) {
+        final BigDecimal quoteAssetQty = orderEvent.getOriginalQuantity()
+                .multiply(orderEvent.getPriceOfLastFilledTrade());
+        final String str = String.format("%s %s %s %s for total of %s quoteAsset",
+                orderEvent.getDateTime().format(ISO_DATE_TIME), orderEvent.getSide(),
+                orderEvent.getOriginalQuantity().toPlainString(), orderEvent.getSymbol(),
+                quoteAssetQty.toPlainString());
+        System.out.println(str);
+    }
 
 }
--- src/main/java/com/example/binanceparser/domain/SpotIncomeState.java
+++ src/main/java/com/example/binanceparser/domain/SpotIncomeState.java
@@ -4,127 +4,130 @@ import com.example.binanceparser.Constants;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
 import static com.example.binanceparser.Constants.*;
 
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 public class SpotIncomeState extends BalanceState {
-	transient private Set<Asset> currentAssets;//transient for tests
-	transient private Set<AssetState> lockedAssetStates;//transient for tests
+    transient private Set<Asset> currentAssets;//transient for tests
+    transient private Set<AssetState> lockedAssetStates;//transient for tests
 
-	public SpotIncomeState(LocalDateTime dateTime) {
-		super(BigDecimal.ZERO, dateTime);
-		currentAssets = new HashSet<>();
-		lockedAssetStates = new HashSet<>();
-	}
-	
-	public SpotIncomeState(BigDecimal conditionedUSDTBalsnce, LocalDateTime dateTime) {
-		super(conditionedUSDTBalsnce, dateTime);
-		currentAssets = new HashSet<>();
-		lockedAssetStates = new HashSet<>();
-	}
-	
-	public SpotIncomeState(LocalDateTime dateTime, SpotIncomeState incomeState) {
-		super(incomeState.getBalanceState(), dateTime);
-		currentAssets = new HashSet<>(incomeState.getCurrentAssets());
-		lockedAssetStates = new HashSet<>(incomeState.getLockedAssetStates());
-	}
+    public SpotIncomeState(LocalDateTime dateTime) {
+        super(BigDecimal.ZERO, dateTime);
+        currentAssets = new HashSet<>();
+        lockedAssetStates = new HashSet<>();
+    }
 
-	public BigDecimal totalBalanceToRelativeAsset() {
-		BigDecimal sum = new BigDecimal(0);
-		for (Asset asset : currentAssets) {
-			if (asset.getAsset().equals(USDT) || asset.getAsset().equals(BUSD)) {
-				sum = sum.add(asset.getAvailableBalance());
-			}
-		}
+    public SpotIncomeState(BigDecimal conditionedUSDTBalsnce, LocalDateTime dateTime) {
+        super(conditionedUSDTBalsnce, dateTime);
+        currentAssets = new HashSet<>();
+        lockedAssetStates = new HashSet<>();
+    }
 
-		for (AssetState assetState : lockedAssetStates) {
-			sum = sum.add(assetState.getAvailableBalance().multiply(assetState.getAveragePrice()));
-		}
-		return sum;
-	}
+    public SpotIncomeState(LocalDateTime dateTime, SpotIncomeState incomeState) {
+        super(incomeState.getBalanceState(), dateTime);
+        currentAssets = new HashSet<>(incomeState.getCurrentAssets());
+        lockedAssetStates = new HashSet<>(incomeState.getLockedAssetStates());
+    }
 
-	public AssetState findAssetState(String assetName) {
-		return lockedAssetStates.stream().filter(a -> a.getAsset().equals(assetName)).findFirst().orElse(null);
-	}
+    public BigDecimal totalBalanceToRelativeAsset() {
+        BigDecimal sum = new BigDecimal(0);
+        for (Asset asset : currentAssets) {
+            if (asset.getAsset().equals(USDT) || asset.getAsset().equals(BUSD)) {
+                sum = sum.add(asset.getAvailableBalance());
+            }
+        }
 
-	public AssetState addAssetIfNotExist(String assetName) {
-		final AssetState assetState = findAssetState(assetName);
-		if(assetState != null) return assetState;
+        for (AssetState assetState : lockedAssetStates) {
+            sum = sum.add(assetState.getAvailableBalance().multiply(assetState.getAveragePrice()));
+        }
+        return sum;
+    }
 
-		final AssetState newAsset = new AssetState(assetName, BigDecimal.ZERO, BigDecimal.ZERO);
-		lockedAssetStates.add(newAsset);
-		return newAsset;
-	}
-	
-	public void updateCurrentAssets(List<Asset> updatedAssets) {
-		updatedAssets.stream().forEach((updatedAsset)->{
-			currentAssets.removeIf((currentAsset) -> currentAsset.getAsset().equals(updatedAsset.getAsset()));
-			currentAssets.add(updatedAsset);
-		});
-	}
-	//TODO  AssetState i BalanceState - була намішана логіка і розмиті сфери відповідальності
-	// публічні методи внутрішнього класу використоувалися для зміни стану зовнішнього обєкта.
-	// - один метод AssetState змінював і себе і зовншій клас одночасно
-	public void updateAssetState(String assetName, BigDecimal assetDelta, BigDecimal transactionPrice) {
-		final AssetState asset = addAssetIfNotExist(assetName);
+    public AssetState findAssetState(String assetName) {
+        return lockedAssetStates.stream().filter(a -> a.getAsset().equals(assetName)).findFirst().orElse(null);
+    }
 
-		if (assetDelta.compareTo(BigDecimal.ZERO) < 0) {
-			//SELL operation
-			//TODO а Ассет то поки що не пропадає із lockedBalanceState, хоча ми його вже продали і роз-лочили кошти
-			setBalanceState(getBalanceState()
-					.add(assetDelta.negate().multiply(transactionPrice))// what we got when sold asset
-					.subtract(assetDelta.negate().multiply(asset.getAveragePrice())));// what we spent when bought asset
-		} else {
-			//BUY operation
-			final BigDecimal transactionQuoteQty = assetDelta.multiply(transactionPrice);
-			final BigDecimal newAssetQty = asset.getAvailableBalance().add(assetDelta);
-			final BigDecimal newCalculatedPrice = asset.totalQuoteAssetValue()
-					.add(transactionQuoteQty)
-					.divide(newAssetQty, Constants.MATH_CONTEXT);
-			asset.setAveragePrice(newCalculatedPrice);
-			asset.setAvailableBalance(newAssetQty);
-		}
-	}
+    public AssetState addAssetIfNotExist(String assetName) {
+        final AssetState assetState = findAssetState(assetName);
+        if (assetState != null) return assetState;
 
-	@Data
-	public static class AssetState extends Asset {
-		//remove_me quoteAsset - той в якому ми відображаємо ціну. baseAsset = BTC, quoteAsset = USD (чи USDT)  - той, яким ми платимо
-		private String quoteAsset;
-		// price of asset relative to relativeAsset. E.g., relativeAsset = USD
-		private BigDecimal averagePrice;
+        final AssetState newAsset = new AssetState(assetName, BigDecimal.ZERO, BigDecimal.ZERO);
+        lockedAssetStates.add(newAsset);
+        return newAsset;
+    }
 
-		public AssetState(String asset, BigDecimal availableBalance, BigDecimal averagePrice) {
-			super(asset, availableBalance);
-			this.averagePrice = averagePrice;
-			this.quoteAsset = USD;
-		}
+    public void updateAssetBalance(List<Asset> updatedAssets) {
+        updatedAssets.forEach((updatedAsset) -> {
+            currentAssets.removeIf((currentAsset) -> currentAsset.getAsset().equals(updatedAsset.getAsset()));
+            currentAssets.add(updatedAsset);
+        });
+    }
 
-		public BigDecimal totalQuoteAssetValue(){
-			return averagePrice.multiply(availableBalance);
-		}
+    //TODO  AssetState i BalanceState - була намішана логіка і розмиті сфери відповідальності
+    // публічні методи внутрішнього класу використоувалися для зміни стану зовнішнього обєкта.
+    // - один метод AssetState змінював і себе і зовншій клас одночасно
+    public void processOrderDetails(String assetName, BigDecimal assetDelta, BigDecimal transactionPrice) {
+        final AssetState asset = addAssetIfNotExist(assetName);
 
-		@Override
-		public boolean equals(Object o) {
-			if (o == this) {
-				return true;
-			}
-			if (!(o instanceof AssetState)) {
-				return false;
-			}
-			AssetState as = (AssetState)o;
-			return this.getAsset().equals(as.getAsset());
-		}
+        if (assetDelta.compareTo(BigDecimal.ZERO) < 0) {
+            //SELL operation
+            //TODO а Ассет то поки що не пропадає із lockedBalanceState, хоча ми його вже продали і роз-лочили кошти
+            setBalanceState(getBalanceState()
+                    .add(assetDelta.negate().multiply(transactionPrice))// what we got when sold asset
+                    .subtract(assetDelta.negate().multiply(asset.getAveragePrice())));// what we spent when bought asset
+        } else {
+            //BUY operation
+            final BigDecimal transactionQuoteQty = assetDelta.multiply(transactionPrice);
+            final BigDecimal newAssetQty = asset.getAvailableBalance().add(assetDelta);
+            final BigDecimal newCalculatedPrice = asset.totalQuoteAssetValue()
+                    .add(transactionQuoteQty)
+                    .divide(newAssetQty, Constants.MATH_CONTEXT);
+            asset.setAveragePrice(newCalculatedPrice);
+            asset.setAvailableBalance(newAssetQty);
+        }
+    }
 
-		@Override
-		public int hashCode() {
-			return this.getAsset().hashCode();
-		}
-	}
+    @Data
+    public static class AssetState extends Asset {
+        //remove_me quoteAsset - той в якому ми відображаємо ціну. baseAsset = BTC, quoteAsset = USD (чи USDT)  - той, яким ми платимо
+        private String quoteAsset;
+        // price of asset relative to relativeAsset. E.g., relativeAsset = USD
+        private BigDecimal averagePrice;
+
+        public AssetState(String asset, BigDecimal availableBalance, BigDecimal averagePrice) {
+            super(asset, availableBalance);
+            this.averagePrice = averagePrice;
+            this.quoteAsset = USD;
+        }
+
+        public BigDecimal totalQuoteAssetValue() {
+            return averagePrice.multiply(availableBalance);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            }
+            if (!(o instanceof AssetState)) {
+                return false;
+            }
+            AssetState as = (AssetState) o;
+            return this.getAsset().equals(as.getAsset());
+        }
+
+        @Override
+        public int hashCode() {
+            return this.getAsset().hashCode();
+        }
+    }
 }
